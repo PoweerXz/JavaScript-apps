d9f01bf9cf94998b9ba7642e3745fb018143d8be
poweerxz <rafaelguadencio@gmail.com>
2022-08-20 22:41:41 -0300

    Refatorando o codigo, fiz novos 2 objetos, um para listar as mensagens do console.log, outro pensando nos operadores

--- apps/calculadora.js
+++ apps/calculadora.js
@@ -1,42 +1,56 @@
 function calculadora(valor1, valor2, operador) {
-    // if (valor1 <= 0) {
-    //     console.log('Não e possivel fazer essa operação')
-    // }
-    // else if (valor2 <= 0) {
-    //     console.log('Não e possivel fazer essa operação')
-    // } else {
-    //     let resultado = valor1 / valor2;
-    //     console.log(resultado)
-    //     return
-    // }
-    if (operador == "*") {
-        console.log('Você escolheu a multiplicação e escolheu os numeros : ', valor1, valor2)
-        let resultadoMutiplicao = valor1 * valor2
-        console.log(resultadoMutiplicao)
-    } else if (operador == '/') {
+    let operadores = {
+    multiplicao: '*',
+    divisao: '/',
+    subtracao: '-',
+    soma: '+',
+    };
+
+    let mensagens = {
+        multiplicacao: 'Você escolheu a multiplicação e escolheu os numeros: ',
+        divisao: 'Você escolheu a divisão e escolheu os numeros : ',
+        subtracao: 'Você escolheu a subtração e escolheu os numeros :',
+        soma: 'Você escolheu a soma e escolheu os numeros : ',
+        erro: 'Operação e invalida',
+
+    }
+
+    if (operador == operadores.multiplicao) {
+
+        console.log(mensagens.multiplicacao, valor1, valor2)
+        let resultadoMultiplicacao = valor1 * valor2
+        
+        console.log(resultadoMultiplicacao)
+    
+    } else if (operador == operadores.divisao) {
+    
         if (valor1 <= 0) {
-            console.log('Não e possivel fazer essa operação')
-        }
-        else if (valor2 <= 0) {
-            console.log('Não e possivel fazer essa operação')
+            console.log(mensagens.erro)
+    
+        } else if (valor2 <= 0) {
+            console.log(mensagens.erro)
         } else {
-            console.log('Você escolheu a divisão e escolheu os numeros :', valor1, valor2)
+            console.log(mensagens.divisao, valor1, valor2)
+            
             let resultadoDivisao = valor1 / valor2
             console.log(resultadoDivisao)
-
         }
-    } else if (operador == '-') {
-        console.log('Você escolheu a subtração e escolheu os numeros :', valor1, valor2)
+    } else if (operador == operadores.subtracao) {
+        console.log(mensagens.subtracao, valor1, valor2)
+        
         let resultadoSubtracao = valor1 - valor2
+        
         console.log(resultadoSubtracao)
-    } else if (operador == '+') {
-        console.log('Você escolheu o operador soma e escolheu os numeros: ', valor1, valor2)
+    } else if (operador == operadores.soma) {
+        console.log(mensagens.soma, valor1, valor2)
+     
         let resultadoSoma = valor1 + valor2
+     
         console.log(resultadoSoma)
-                  } else {
-        console.log('A operação e invalida')
+    } else {
+        console.log(mensagens.erro)
     }
 
 }
 
-calculadora(10, 0, "/")
+calculadora(30, 10, "+")
